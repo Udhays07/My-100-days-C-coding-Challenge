ef7f92f26558cb723f66938c062a92785929b708
Udhaya Moorthy <149321495+Udhays07@users.noreply.github.com>
2024-03-14 13:52:53 +0530

    Create Day10.c
    Output:
    Enter the value of length and width:< values to be given as input >
     The area of the given Rectangle is: < area of rectangle >

---
+++ Day10.c
@@ -0,0 +1,12 @@
+//Write a program to find Area of a rectangle.
+#include<stdio.h>
+int main()
+{
+    float l, b; // input is taken in float for the accurate output
+    float area;
+    printf("Enter the value of length and bredth: ");
+    scanf("%f %f",&l,&b);
+    area = l*b;
+    printf("The area of the given Rectangle is: %f", area);
+    return 0;
+}
