181444e661c0e3619a62639336822e592010e4be
Udhaya Moorthy <149321495+Udhays07@users.noreply.github.com>
2024-03-14 13:53:21 +0530

    Update Day10.c

--- Day10.c
+++ Day10.c
@@ -4,7 +4,7 @@ int main()
 {
     float l, b; // input is taken in float for the accurate output
     float area;
-    printf("Enter the value of length and bredth: ");
+    printf("Enter the value of length and width: ");
     scanf("%f %f",&l,&b);
     area = l*b;
     printf("The area of the given Rectangle is: %f", area);
