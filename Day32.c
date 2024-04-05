59a28f6573fd18c91ce06e5194326db81e7bf0be
Udhaya Moorthy <149321495+Udhays07@users.noreply.github.com>
2024-04-05 20:46:53 +0530

    Create Day32.c

---
+++ Day32.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int replace(long int number){
+    if (number == 0)
+    return 0;
+    int digit = number % 10;
+    if (digit == 0)
+    digit = 1;
+    return replace(number/10) * 10 + digit;
+}
+int Convert(long int number){
+    if (number == 0)
+    return 1;
+    else
+    return replace(number);
+}
+int main(){
+    long int number;
+    printf("\nEnter any number : ");
+    scanf("%d", &number);
+    printf("\nAfter replacement the number is : %d", Convert(number));
+    return 0;
+}
