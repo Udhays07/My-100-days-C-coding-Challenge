04d161ca47735dd28e7c09aa1625c435697c6c36
Udhaya Moorthy <149321495+Udhays07@users.noreply.github.com>
2024-04-07 18:52:10 +0530

    Create Day34.c

---
+++ Day34.c
@@ -0,0 +1,29 @@
+//Write a program to print Pyramid pattern using numbers
+#include<stdio.h>
+int main()
+{
+    int i, space, rows, k=0, count = 0, count1=0;
+    printf("Enter the number of rows for Pyramid: ");
+    scanf("%d",&rows);
+    for(i=1; i<=rows; ++i)
+    {
+        for(space=1; space<=rows-i;++space)
+        {
+            printf("  ");
+            ++count;
+        }
+        while(k!=2*i-1){
+            if(count<=rows-1){
+                printf("%d ",i+k);
+                ++count;
+            }else{
+                ++count1;
+                printf("%d ",(i+k-2*count1));
+            }
+            ++k;
+        }
+        count1 = count = k =0;
+        printf("\n");
+    }
+    return 0;
+}
