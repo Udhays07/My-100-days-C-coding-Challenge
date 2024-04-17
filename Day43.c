f9b50acd098134f1c5743e069a13fa9fe1827b23
Udhaya Moorthy <149321495+Udhays07@users.noreply.github.com>
2024-04-17 11:40:10 +0530

    Update Day43.c

--- Day43.c
+++ Day43.c
@@ -1,39 +1,18 @@
+//Write a program for binary to Octal conversion
 #include <stdio.h>
-#define MAX 1000
- 
 int main()
 {
-    char octalnum[MAX];
-    long i = 0;
+    long int binarynum, octalnum = 0, j = 1, remainder;
  
-    printf("Enter any octal number: ");
-    scanf("%s", octalnum);
-    printf("Equivalent binary value: ");
-    while (octalnum[i])
+    printf("Enter the value for  binary number: ");
+    scanf("%ld", &binarynum);
+    while (binarynum != 0)
     {
-        switch (octalnum[i])
-        {
-        case '0':
-            printf("000"); break;
-        case '1':
-            printf("001"); break;
-        case '2':
-            printf("010"); break;
-        case '3':
-            printf("011"); break;
-        case '4':
-            printf("100"); break;
-        case '5':
-            printf("101"); break;
-        case '6':
-            printf("110"); break;
-        case '7':
-            printf("111"); break;
-        default:
-            printf("\n Invalid octal digit %c ", octalnum[i]);
-            return 0;
-        }
-        i++;
+        remainder = binarynum % 10;
+        octalnum = octalnum + remainder * j;
+        j = j * 2;
+        binarynum = binarynum / 10;
     }
+    printf("Equivalent octal value: %lo", octalnum);
     return 0;
 }
